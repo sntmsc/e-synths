815ec3b9916ab15a831a8a2a0d78ba797ee5d35f
sntmsc <80355751+sntmsc@users.noreply.github.com>
2022-03-27 15:47:26 -0300

    navbar fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -20,6 +20,7 @@ import {Flex,
 
 const Navbar = ({isMobile}) => {
    const dispatch = useDispatch();
+
     const Logo = () => { 
       return(
         <Link to={'/'} style={{textDecoration: 'none'}}>
@@ -46,6 +47,7 @@ const Navbar = ({isMobile}) => {
           </Link>
       )
     }
+
       const Buscador = () =>{
         const dispatch = useDispatch();
           const handleSearch = (e) =>{
@@ -55,13 +57,11 @@ const Navbar = ({isMobile}) => {
             e.target.text.value = "";
           }
         return(
-          
+
           <Flex 
           m={{base:"1em",md:"1.5em",lg:"2em"}}
-          w={{base:"85%",md:"40%"}}
-          justify="center" 
-          position="relative" 
-          left={{base:"1%",lg:'5%', xl:'10%', '2xl':'18%'}}>
+          w={{base:"85%",md:"15em"}}
+          justify="center">
             <form onSubmit={handleSearch} style={{display: 'flex'}}>
               <Input
               mr=".5em"
@@ -84,7 +84,7 @@ const Navbar = ({isMobile}) => {
             categoria:'Samplers/Groovebox',
             link:'/groovebox'}]
         return(
-          <Stack spacing={5} ml={{md:'8',lg:'5'}} position='relative' right='20%'>
+          <Stack spacing={5} ml={{md:'8',lg:'5'}}>
           {descripcion!=='Productos' && 
                   <Link to={link} style={{textDecoration: 'none', margin:'0'}}>
                     <Flex 
@@ -158,8 +158,6 @@ const Navbar = ({isMobile}) => {
                     aria-label="Options"
                     icon={<HamburgerIcon boxSize="1.7em"/>}
                     variant="ghost"
-                    position="relative"
-                    left={{base:"73%", sm:'78%', md:'87%', lg:'95%'}}
                 />
                 <MenuList w="20em" h="10em" bg="gray.200">
                   <Link to={'/groovebox'} style={{textDecoration: 'none'}}>
@@ -196,42 +194,47 @@ const Navbar = ({isMobile}) => {
     >
       <Logo/>
       <Flex 
-      w="65%"
       h="100%"
       align="center"
+      justify='space-between'
       >
         {!isMobile && 
         <>
-        <Session descripcion={'Productos'}/>
-        <Session descripcion={'Nosotros'} link={'/nosotros'}/>
-        <Session descripcion={'Contactanos'} link={'/contacto'}/>
-        <Buscador/>
-        <Image
-        src='/icons/carrito.png'
-        alt='carrito de compras' 
-        objectFit='cover'
-        position='relative'
-        left={{xl:'1%','2xl':'10%'}}
-        boxSize='2em'
-        mr='.5em'
-        cursor='pointer'
-        onClick={()=>dispatch(toggleCarrito())}/>
+        <Flex
+        justify='flex-start'
+        align='center'>
+          <Session descripcion={'Productos'}/>
+          <Session descripcion={'Nosotros'} link={'/nosotros'}/>
+          <Session descripcion={'Contactanos'} link={'/contacto'}/>
+        </Flex>
+        <Flex
+        justify='flex-end'
+        align='center'>
+          <Buscador/>
+          <Image
+          src='/icons/carrito.png'
+          alt='carrito de compras' 
+          objectFit='cover'
+          boxSize='2em'
+          mr='.5em'
+          cursor='pointer'
+          onClick={()=>dispatch(toggleCarrito())}/>
+        </Flex>
         </>
         }
         {isMobile && 
         <>
-
-          <MenuMobile/>
-          <Image
-              src='/icons/carrito.png'
-              alt='carrito de compras' 
-              objectFit='cover'
-              boxSize='2em'
-              position='relative'
-              left={{base:'30%',sm:'45%',md:'50%', lg:'70%',xl:'80%'}}
-              m='0'
-              cursor='pointer'
-              onClick={()=>dispatch(toggleCarrito())}/>
+          <Flex>
+            <MenuMobile/>
+            <Image
+                src='/icons/carrito.png'
+                alt='carrito de compras' 
+                objectFit='cover'
+                boxSize='2em'
+                m='0'
+                cursor='pointer'
+                onClick={()=>dispatch(toggleCarrito())}/>
+            </Flex>
         </>
         }
       </Flex>
