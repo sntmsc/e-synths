ba59fa578ff7ce3a0be92e672e026e2d9d97510f
sntmsc <80355751+sntmsc@users.noreply.github.com>
2022-03-29 11:43:16 -0300

    add routes in category links

--- src/components/home/Categorias.js
+++ src/components/home/Categorias.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Flex,Stack,Text,Image} from "@chakra-ui/react" 
+import {Link} from 'react-router-dom'
 
 
 const Categorias = () =>{
@@ -7,13 +8,16 @@ const Categorias = () =>{
     const infoCategorias = [
       {
       nombre:'Sintetizadores',
-      img:'./imgs/productos/sintetizadores/microbrute.jpg'
+      img:'./imgs/productos/sintetizadores/microbrute.jpg',
+      route:'/sintetizadores'
     },{
       nombre:'Drum machines',
-      img:'./imgs/productos/drummachines/808.jpg'
+      img:'./imgs/productos/drummachines/808.jpg',
+      route:'/drums'
     },{
       nombre:'Samplers/Groovebox',
-      img:'./imgs/productos/groovebox/mpc2000xl.jpg'
+      img:'./imgs/productos/groovebox/mpc2000xl.jpg',
+      route:'/groovebox'
     }]
   
     return(
@@ -37,33 +41,35 @@ const Categorias = () =>{
         justify='center'
         direction={{base:'column',md:'row'}}>
             {infoCategorias.map((x,i) =>
-             <Flex
-            direction='column'
-            key={i}
-            alignItems='center'
-            justify='center'
-            boxSize='15em'
-            position='relative'> 
-  
-                <Image 
-                h='8em'
-                w='9em'
-                borderRadius='50%'
-                border='1px solid black'
-                borderColor='rgba(0,0,0,0.2)'
-                mb='1em'
-                src={x.img}
-                alt={`Categoría ${x.nombre}`}
-                objectFit='cover'
-                _hover={{h:'9em',w:'10em',cursor:'pointer'}}/>
-                
-              <Text
-              fontSize='1.5em'
-              fontFamily=" 'Poppins', sans-serif;"
-              position='absolute'
-              bottom='0em'
-              >{x.nombre}</Text>
-            </Flex>)}
+            <Link to={x.route} style={{textDecoration: 'none'}}>
+              <Flex
+              direction='column'
+              key={i}
+              alignItems='center'
+              justify='center'
+              boxSize='15em'
+              position='relative'> 
+    
+                  <Image 
+                  h='8em'
+                  w='9em'
+                  borderRadius='50%'
+                  border='1px solid black'
+                  borderColor='rgba(0,0,0,0.2)'
+                  mb='1em'
+                  src={x.img}
+                  alt={`Categoría ${x.nombre}`}
+                  objectFit='cover'
+                  _hover={{h:'9em',w:'10em',cursor:'pointer'}}/>
+                  
+                <Text
+                fontSize='1.5em'
+                fontFamily=" 'Poppins', sans-serif;"
+                position='absolute'
+                bottom='0em'
+                >{x.nombre}</Text>
+              </Flex>
+            </Link>)}
         </Stack>
       </Flex>
     )
